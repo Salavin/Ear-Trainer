e9660f7dd7478f00597cabd569c2b6b26e33fe5d
Sam Lavin <salavin12@gmail.com>
2022-12-02 12:29:06 -0600

    Completed tone function

--- app/src/main/java/in/samlav/eartrainer/TestFragment.kt
+++ app/src/main/java/in/samlav/eartrainer/TestFragment.kt
@@ -1,16 +1,23 @@
 package `in`.samlav.eartrainer
 
+import `in`.samlav.eartrainer.databinding.FragmentTestBinding
+import android.content.SharedPreferences
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioTrack
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import `in`.samlav.eartrainer.databinding.FragmentTestBinding
-import android.content.SharedPreferences
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceManager
+import kotlin.math.sin
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -26,6 +33,9 @@ class TestFragment : Fragment()
     private lateinit var preferences: SharedPreferences
     private var numBins = 0
     private lateinit var array: Array<out String>
+
+    private val sampleRate = 8000
+    private lateinit var generatedSnd: ByteArray
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -75,6 +85,11 @@ class TestFragment : Fragment()
             }
             binding.root.findViewById<LinearLayout>(R.id.bin_container).addView(button)
         }
+
+        // TODO: Test Logic
+
+        playTone(440, 25)
+
     }
 
     override fun onDestroyView()
@@ -86,5 +101,51 @@ class TestFragment : Fragment()
     private fun buttonPress(freq: Int)
     {
         Toast.makeText(context, "You pressed: $freq", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun playTone(freq: Int, length: Int)
+    {
+        Thread {
+            genTone(freq, length)
+            Handler(Looper.getMainLooper()).post { playSound() }
+        }.start()
+    }
+
+    private fun genTone(freq: Int, duration: Int)
+    {
+        val seconds = duration.toFloat() / 100f
+        val numSamples = (seconds * sampleRate).toInt()
+        generatedSnd = ByteArray(2 * numSamples)
+        val sample = DoubleArray(numSamples)
+
+        // fill out the array
+        for (i in 0 until numSamples)
+        {
+            sample[i] = sin(2 * Math.PI * i / (sampleRate / freq))
+        }
+
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalised.
+        var idx = 0
+        for (dVal in sample)
+        {
+            // scale to maximum amplitude
+            val `val` = (dVal * 32767).toInt()
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (`val` and 0x00ff).toByte()
+            generatedSnd[idx++] = ((`val` and 0xff00) ushr 8).toByte()
+        }
+    }
+
+    private fun playSound()
+    {
+        val audioTrack = AudioTrack(
+            AudioManager.STREAM_MUSIC,
+            sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_16BIT, generatedSnd.size,
+            AudioTrack.MODE_STATIC
+        )
+        audioTrack.write(generatedSnd, 0, generatedSnd.size)
+        audioTrack.play()
     }
 }
